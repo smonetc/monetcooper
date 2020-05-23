702c533a3c986acbceeaf623591685a4386b6d4f
Monet Cooper <smomoc@Monets-MacBook-Pro-3.local>
2020-05-23 02:43:23 -0400

    updated js

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 "use strict";
 
-function myFunction() {
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btnText = document.getElementById("myBtn");
+
+// this function enables a read more/read less button
+
+// function myFunction() {
+//     let dots = document.getElementById("dots");
+//     let moreText = document.getElementById("more");
+//     let btnText = document.getElementById("myBtn");
   
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
-  }
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read more";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read less";
+//       moreText.style.display = "inline";
+//     }
+//   }
